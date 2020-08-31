0a3a43e3a5cb4fffdf02095a339eb080cc70aabc
robertlovin23 <robert.a.lovin@gmail.com>
2020-08-31 18:23:40 -0500

    new commit

--- index.js
+++ index.js
@@ -61,18 +61,18 @@ app.get('/github/repository/:owner/:repo', cache(10), async (req,res) => {
     res.send(repo.data)
 })
 
-// if(process.env.NODE_ENV === 'production'){
-//     // Express will serve up production assets
-//     // like our main.js or main.css
-//     app.use(express.static('client/build'));
-//     // Express will serve up index.html file
-//     // if it doesn't recognizes the route
-//     const path = require('path');
+if(process.env.NODE_ENV === 'production'){
+    // Express will serve up production assets
+    // like our main.js or main.css
+    app.use(express.static('client/build'));
+    // Express will serve up index.html file
+    // if it doesn't recognizes the route
+    const path = require('path');
 
-//     app.get('*', (req,res) => {
-//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-//     });
-// }
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const port = process.env.PORT || 5000;
 
