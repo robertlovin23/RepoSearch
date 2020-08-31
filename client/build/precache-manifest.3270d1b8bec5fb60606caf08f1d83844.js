f339cfc718c248cc816488b24cb9e337a5b4a0be
robertlovin23 <robert.a.lovin@gmail.com>
2020-08-31 18:32:20 -0500

    new commit

---
+++ client/build/precache-manifest.3270d1b8bec5fb60606caf08f1d83844.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72d5cf792d9a19d27b020b606bfc6599",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b35bd8aedc9d167db1f",
+    "url": "/static/js/2.671b94b8.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/static/js/2.671b94b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a91cb966335580d91e8c",
+    "url": "/static/js/main.0fd1770b.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
